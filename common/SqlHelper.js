c64d08fe7e35b981b29c69f6fa5a9fbf1714dc27
Kaushik <dhameliyakaushik13@gmail.com>
2018-02-13 16:14:06 +0530

    Initial commit

---
+++ common/SqlHelper.js
@@ -0,0 +1,28 @@
+﻿var config = require("./../config");
+var sql = require("mssql");
+
+exports.executeStoredProcedure = function (query, callback) {
+    sql.connect(config.storage.pool).then(pool => {
+        pool.request().execute(query, (err, result) => {
+            sql.close();
+            callback(err, result);
+        }).catch(function (err) {
+            sql.close();
+            console.log(err);
+            callback(err, null);
+        });;
+    });
+}
+
+exports.executeQuery = function (query, callback) {
+    sql.connect(config.storage.pool).then(pool => {
+        pool.request().query(query, (err, result) => {
+            sql.close();
+            callback(err, result);
+        }).catch(function (err) {
+            sql.close();
+            console.log(err);
+            callback(err, null);
+        });;
+    });
+}
---
+++ controllers/Currency.js
@@ -0,0 +1,44 @@
+﻿var express = require("express");
+var router = express.Router();
+var db = require("./../common/SqlHelper");
+
+router.get("/", function (objCountry, res) {
+    db.executeStoredProcedure("GetAllCurrency", function (err, result) {
+        if (err)
+            res.send(JSON.stringify(err));
+        else
+            res.send(JSON.stringify(result.recordsets));
+    });
+});
+
+router.get("/GetCountry/:id", function (objCountry, res) {
+    const getCountryById = "Select * From CurrencyMaster Where CurrId = '" + objCountry.params.id + "'";
+    db.executeQuery(getCountryById, function (err, result) {
+        if (err)
+            res.send(JSON.stringify(err));
+        else
+            res.send(JSON.stringify(result.recordsets));
+    });
+});
+
+router.post("/AddCountry", function (objCountry, res) {
+    const adddetail = "insert into CurrencyMaster(CurrName, Symbol, IsActive, IsMain) VALUES ('" + objCountry.body.CountryName + "', '" + objCountry.body.Symbol + "', 1, 1)";
+    db.executeQuery(adddetail, function (err, result) {
+        if (err)
+            res.send(JSON.stringify(err));
+        else
+            res.send(JSON.stringify(result.recordsets));
+    });
+});
+
+router.post("/DeleteCountry", function (objCountry, res) {
+    const deleteQuery = "Delete From CurrencyMaster Where CurrId = '" + objCountry.body.CurrId + "'";
+    db.executeQuery(deleteQuery, function (err, result) {
+        if (err)
+            res.send(JSON.stringify(err));
+        else
+            res.send(JSON.stringify(result.recordsets));
+    });
+});
+
+module.exports = router;
