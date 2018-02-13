02ffc728bab64bdd1b8633b1acd0288934910ebb
Kaushik <dhameliyakaushik13@gmail.com>
2018-02-13 18:03:51 +0530

    send email

---
+++ common/Common.js
@@ -0,0 +1,16 @@
+﻿var nodemailer = require("nodemailer");
+
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "email address",
+        pass: "password"
+    }
+});
+
+exports.sendEmail = function (mailOptions, callback) {
+    smtpTransport.sendMail(mailOptions, function (response, err) {
+        callback(err, result);
+    });
+}
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,6 +6,8 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var verifyToken = require('./../common/VerifyToken');
 var config = require("./../config");
+var common = require('./../common/Common');
+//var nodemailer = require("nodemailer");
 
 router.post("/login", function (req, res) {
     // create a token
@@ -25,6 +27,40 @@ router.get('/me', verifyToken, function (req, res, next) {
     res.status(200).send("Ok");
 });
 
+//var smtpTransport = nodemailer.createTransport({
+//    service: "gmail",
+//    host: "smtp.gmail.com",
+//    auth: {
+//        user: "YOUR EMAIL HERE",
+//        pass: "YOUR PASSWORD HERE"
+//    }
+//});
+
+router.post('/sendmail', function (req, res) {
+    var mailOptions = {
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.text
+    }
+
+    common.sendEmail(mailOptions, function (err, result) {
+        if (err)
+            res.send(JSON.stringify(err));
+        else
+            res.send(JSON.stringify(result.recordsets));
+    });
+
+    //console.log(mailOptions);
+    //smtpTransport.sendMail(mailOptions, function (error, response) {
+    //    if (error) {
+    //        console.log(error);
+    //        res.end("error");
+    //    } else {
+    //        console.log("Message sent: " + response.message);
+    //        res.end("sent");
+    //    }
+    //});
+});
 
 
 module.exports = router;
